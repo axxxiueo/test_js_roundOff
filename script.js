b4197694ce1de17f0073f69079f5e7139fcd48db
axxxiueo <77669299+axxxiueo@users.noreply.github.com>
2023-10-01 20:47:47 +0900

    completed

---
+++ script.js
@@ -0,0 +1,18 @@
+// 切り捨て・切り上げ・四捨五入
+const number = 123.56
+
+// 切り捨て
+console.log('floor',Math.floor(number))
+console.log('trunc',Math.trunc(number))
+
+// 切り上げ
+console.log('ceil',Math.ceil(number))
+
+// 四捨五入
+console.log('round',Math.round(number))
+
+// 絶対値
+console.log('abs',Math.abs(number))
+
+// べき乗
+console.log('pow',Math.pow(number,3))
